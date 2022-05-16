0b908d13c447bc769ba3113889bb24fb0e7018dd
annk1995 <kabuikariuki052@gmail.com>
2022-05-17 00:15:54 +0300

    added

---
+++ start.sh
@@ -0,0 +1,7 @@
+export EMAIL_USER='kabui.kariuki@student.moringaschool.com
+'
+    export EMAIL_PASS='kariuki@com'
+    export SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+    export SECRET_KEY='f9bf78b9a18ce6d46a0cd2b0b86df9da'
+
+    python3 manager.py server
