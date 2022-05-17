c1d6ff9bc5502fa9bd46f7e5c5b25568e622edec
annk1995 <kabuikariuki052@gmail.com>
2022-05-17 23:06:50 +0300

    added st

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@
   
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+from flask.ext.bcrypt import Bcrypt
+# from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from app.config import Config
